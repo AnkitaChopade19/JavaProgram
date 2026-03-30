89f17141d2186ade0071812e8011255ded62169e
AnkitaChopade19 <ankitachopade302@gmail.com>
2026-03-30 17:05:20 +0530

    Add files via upload

---
+++ cls/Employee.java
@@ -0,0 +1,17 @@
+package com.cls;
+
+public class Employee {
+	
+	public static void main(String args[]) {
+		
+		emp e1 = new emp();
+		e1.empId = 21;
+		e1.empName = "Ankita";
+		e1.city = "Solapur";
+		e1.salary = 200000 ;
+		System.out.println(e1.empId + " " + e1.empName + " " + e1.city + " " + e1.salary);
+		
+		
+	}
+
+}
---
+++ cls/emp.java
@@ -0,0 +1,10 @@
+package com.cls;
+
+public class emp {
+	
+	int empId;
+	String empName;
+	String city;
+	double salary;
+
+}
