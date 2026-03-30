e1b4ad315cc60320890c271a4f8fa087b9ce7972
AnkitaChopade19 <ankitachopade302@gmail.com>
2026-03-30 17:08:29 +0530

    Add files via upload

---
+++ cons/MainConstructorDay2.java
@@ -0,0 +1,37 @@
+package com.cons;
+
+public class MainConstructorDay2 {
+public static void main(String args[]) {
+	
+	
+	//------------------------Information About Student------------------------------------
+/*	Constructorday2 c1 = new ConstructorDay2(123 , "Shruti ",  21 , "Pune");
+	c1.viewInffo();
+	//c1.studentName = "Suru";
+	c1.viewInffo();
+
+	
+	//----------------------Information About Employee----------------------------------------
+	ConstructorDay2 c1 = new ConstructorDay2(101 , "Vijay" ,"Latur" , 90000l , 8767999099l );
+	c1.display();
+	
+	
+	//---------------------My Portfolio---------------------------------------------------
+	ConstructorDay2  c = new ConstructorDay2 ("Shruti Barmade" ,06 , 21 , "TE IT Third Year " , "Siddhant Collage Of Engineering Pune", 8.76f , "Latur");
+	c.display();
+	
+	
+
+	//------------Information About Car-------------------------------------------------------
+	ConstructorDay2  c = new ConstructorDay2("Swift" , "Suzuki" ,  2022 ,"White",5, 800000l);
+	c.display();   */
+	
+	
+	//--------------Information About Collage---------------------------------------
+	ConstructorDay2  c = new ConstructorDay2("Siddhant Collage Of Engineering" ,       "Sudumbare,Pune" ,  2005 ,"Private Engineering Collage","Savitribai Phule Pune University","AICTE Approved",600,58500);
+	c.display();
+	
+	
+	
+}
+}
---
+++ cons/MainConstructorTask.java
@@ -0,0 +1,32 @@
+package com.cons;
+
+public class MainConstructorTask {
+
+public static void main(String args[]) {
+	
+/*	/----------create an class with Name Student --> create an constructor and print whole info about Student---
+	ConstructorTask s = new ConstructorTask("Riya" , "Bhosale" , 21, 8767901330l , "Female" , "riyabhosale@gmail.com ", "BE");
+		s.city("Pune");
+	
+	
+	
+//------------create an class with Name myPortFolio --> create an constructor and print whole info about u----------
+	ConstructorTask s = new ConstructorTask("Shruti", 21, "Female" , "Latur" , "Maharashtra" , "India" , "BE" , "Siddhant Collage Of Engineering , Pune " , 8767901030l ,"shrutibaramde05@gmail.com");
+	
+
+	
+//-----------create an class with Name Car --> create an constructor and print whole info about car and also add one method-----------
+	
+	ConstructorTask  c = new ConstructorTask ("Swift" , "Suzuki" , 2022, "White" , 5 , 800000l);
+	c.car(1234);
+	*/	
+	
+	
+//----------create an class with Name Employee --> create an constructor and print whole info about employee also add method (salary)------------
+		
+	ConstructorTask  c = new ConstructorTask (123 , "Vijay Shivare" , 25 ,"Male" , 8767901030l ,"Pune" ,"vijayshivare@gmail.com");
+	c.Employee(50000);
+	}
+}
+
+
